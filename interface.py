b2ddc460802f92ef5f87df00bab282ae55edcb38
ornellakmc <ornella95190@gmail.com>
2025-05-06 21:41:14 +0200

    o

--- interface.py
+++ interface.py
@@ -66,20 +66,75 @@ def sauvegarder_image():
             photo_affichee.save(chemin)
 
 def lancer_interface():
+    import tkinter as tk
+    from tkinter import filedialog, messagebox
+    from PIL import Image, ImageTk
+    from filtres import filtre_flou_uniforme
+
+    # Variables internes
+    photo_originale = None
+    photo_affichee = None
+    historique = []
+    indice_historique = -1
+
+    def ouvrir_image():
+        nonlocal photo_originale, photo_affichee, historique, indice_historique
+        chemin = filedialog.askopenfilename(title="Choisir une image", filetypes=[("Images", "*.jpg *.png *.jpeg")])
+        if chemin:
+            photo_originale = Image.open(chemin).convert("RGB")
+            photo_affichee = photo_originale.copy()
+            historique = [photo_affichee.copy()]
+            indice_historique = 0
+            afficher_image()
+
+    def afficher_image():
+        nonlocal image_label, photo_affichee
+        if photo_affichee:
+            tk_image = ImageTk.PhotoImage(photo_affichee)
+            image_label.configure(image=tk_image)
+            image_label.image = tk_image
+
+    def appliquer_filtre(filtre):
+        nonlocal photo_affichee, historique, indice_historique
+        if photo_affichee:
+            historique[:] = historique[:indice_historique + 1]
+            photo_affichee = filtre(historique[indice_historique].copy())
+            historique.append(photo_affichee.copy())
+            indice_historique += 1
+            afficher_image()
+
+    def annuler():
+        nonlocal indice_historique, photo_affichee
+        if indice_historique > 0:
+            indice_historique -= 1
+            photo_affichee = historique[indice_historique].copy()
+            afficher_image()
+
+    def retablir():
+        nonlocal indice_historique, photo_affichee
+        if indice_historique < len(historique) - 1:
+            indice_historique += 1
+            photo_affichee = historique[indice_historique].copy()
+            afficher_image()
+
+    def sauvegarder_image():
+        if photo_affichee:
+            chemin = filedialog.asksaveasfilename(defaultextension=".png", filetypes=[("PNG", "*.png"), ("JPEG", "*.jpg")])
+            if chemin:
+                photo_affichee.save(chemin)
+
+    # Création de la fenêtre principale
     fenetre = tk.Tk()
+    fenetre.title("UVSQolor")
     fenetre.lift()
     fenetre.attributes('-topmost', True)
     fenetre.after(100, lambda: fenetre.attributes('-topmost', False))
-    global image_label
-    print("Lancement de l'interface...")
-    fenetre = tk.Tk()
-    fenetre.title("UVSQolor")
 
-    # Créer le menu
+    # Menu principal
     menu = tk.Menu(fenetre)
     fenetre.config(menu=menu)
 
-    # Menu fichier
+    # Menu Fichier
     fichier_menu = tk.Menu(menu, tearoff=0)
     fichier_menu.add_command(label="Ouvrir", command=ouvrir_image)
     fichier_menu.add_command(label="Sauvegarder", command=sauvegarder_image)
@@ -87,18 +142,18 @@ def lancer_interface():
     fichier_menu.add_command(label="Quitter", command=fenetre.quit)
     menu.add_cascade(label="Fichier", menu=fichier_menu)
 
-    # Menu édition
+    # Menu Édition
     edition_menu = tk.Menu(menu, tearoff=0)
     edition_menu.add_command(label="Annuler", command=annuler)
     edition_menu.add_command(label="Rétablir", command=retablir)
     menu.add_cascade(label="Édition", menu=edition_menu)
 
-    # Menu filtres
+    # Menu Filtres
     filtre_menu = tk.Menu(menu, tearoff=0)
     filtre_menu.add_command(label="Flou uniforme", command=lambda: appliquer_filtre(filtre_flou_uniforme))
     menu.add_cascade(label="Filtres", menu=filtre_menu)
 
-    # Label pour afficher l'image
+    # Zone d'affichage de l'image
     image_label = tk.Label(fenetre)
     image_label.pack()
 
