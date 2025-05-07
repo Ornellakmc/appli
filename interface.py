a17c940188c17caeecba4399acb0db0217410d0a
ornellakmc <ornella95190@gmail.com>
2025-05-07 10:53:24 +0200

    o

--- interface.py
+++ interface.py
@@ -1,35 +1,23 @@
-# interface.py
 import tkinter as tk
 from tkinter import filedialog, messagebox
 from PIL import Image, ImageTk
 from filtres import filtre_flou_uniforme
+import os
 
 # Variables globales
-image_label = None
 photo_originale = None
 photo_affichee = None
 historique = []
 indice_historique = -1
+image_label = None
 
 def afficher_image():
     global image_label, photo_affichee
     if photo_affichee:
         tk_image = ImageTk.PhotoImage(photo_affichee)
         image_label.configure(image=tk_image)
-        image_label.image = tk_image  # Nécessaire pour empêcher que l'image disparaisse
-
-def ouvrir_image():
-    global photo_originale, photo_affichee, historique, indice_historique
-    chemin = filedialog.askopenfilename(
-        title="Choisir une image",
-        filetypes=[("Images", "*.jpg *.jpeg *.png")]
-    )
-    if chemin:
-        photo_originale = Image.open(chemin).convert("RGB")
-        photo_affichee = photo_originale.copy()
-        historique = [photo_affichee.copy()]
-        indice_historique = 0
-        afficher_image()
+        image_label.image = tk_image  # Empêche le garbage collection
+        image_label.update_idletasks()
 
 def appliquer_filtre(filtre):
     global photo_affichee, historique, indice_historique
@@ -57,22 +45,46 @@ def retablir():
 def sauvegarder_image():
     global photo_affichee
     if photo_affichee:
-        chemin = filedialog.asksaveasfilename(
-            defaultextension=".png",
-            filetypes=[("PNG", "*.png"), ("JPEG", "*.jpg")]
-        )
+        chemin = filedialog.asksaveasfilename(defaultextension=".png", filetypes=[("PNG", "*.png"), ("JPEG", "*.jpg")])
         if chemin:
             photo_affichee.save(chemin)
+
+def ouvrir_image():
+    global photo_originale, photo_affichee, historique, indice_historique
+    chemin = filedialog.askopenfilename(title="Choisir une image", filetypes=[("Images", "*.jpg *.png *.jpeg")])
+    if chemin:
+        photo_originale = Image.open(chemin).convert("RGB")
+        photo_affichee = photo_originale.copy()
+        historique = [photo_affichee.copy()]
+        indice_historique = 0
+        afficher_image()
+
+def charger_image_par_defaut():
+    global photo_originale, photo_affichee, historique, indice_historique
+    chemin = "images/img.jpg"
+    if os.path.exists(chemin):
+        photo_originale = Image.open(chemin).convert("RGB")
+        photo_affichee = photo_originale.copy()
+        historique = [photo_affichee.copy()]
+        indice_historique = 0
+        afficher_image()
+    else:
+        print("Aucune image par défaut trouvée.")
 
 def lancer_interface():
     global image_label
 
     fenetre = tk.Tk()
     fenetre.title("UVSQolor")
+    fenetre.lift()
+    fenetre.attributes('-topmost', True)
+    fenetre.after(100, lambda: fenetre.attributes('-topmost', False))
 
+    # Menu principal
     menu = tk.Menu(fenetre)
     fenetre.config(menu=menu)
 
+    # Menu Fichier
     fichier_menu = tk.Menu(menu, tearoff=0)
     fichier_menu.add_command(label="Ouvrir", command=ouvrir_image)
     fichier_menu.add_command(label="Sauvegarder", command=sauvegarder_image)
@@ -80,16 +92,23 @@ def lancer_interface():
     fichier_menu.add_command(label="Quitter", command=fenetre.quit)
     menu.add_cascade(label="Fichier", menu=fichier_menu)
 
+    # Menu Édition
     edition_menu = tk.Menu(menu, tearoff=0)
     edition_menu.add_command(label="Annuler", command=annuler)
     edition_menu.add_command(label="Rétablir", command=retablir)
     menu.add_cascade(label="Édition", menu=edition_menu)
 
+    # Menu Filtres
     filtre_menu = tk.Menu(menu, tearoff=0)
     filtre_menu.add_command(label="Flou uniforme", command=lambda: appliquer_filtre(filtre_flou_uniforme))
     menu.add_cascade(label="Filtres", menu=filtre_menu)
 
+    # Zone d'affichage
     image_label = tk.Label(fenetre)
     image_label.pack()
 
+    # Charger automatiquement une image si elle existe
+    charger_image_par_defaut()
+
+    print("Interface prête.")
     fenetre.mainloop()
