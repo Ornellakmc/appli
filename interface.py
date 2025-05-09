21aac0a30a51b0c6d11db1e68129b2596d9c0a68
ornellakmc <ornella95190@gmail.com>
2025-05-09 10:45:26 +0200

    o

--- interface.py
+++ interface.py
@@ -1,16 +1,9 @@
 import tkinter as tk
-from tkinter import filedialog, messagebox
+from tkinter import filedialog
 from PIL import Image, ImageTk
-from filtres import (
-    filtre_flou_uniforme,
-    filtre_niveaux_de_gris,
-    filtre_negatif,
-    filtre_sepia,
-    filtre_contraste
-)
+from filtres import filtre_flou_uniforme, filtre_rehaussement
 import os
 
-# Variables globales
 photo_originale = None
 photo_affichee = None
 historique = []
@@ -22,17 +15,26 @@ def afficher_image():
     if photo_affichee:
         tk_image = ImageTk.PhotoImage(photo_affichee)
         image_label.configure(image=tk_image)
-        image_label.image = tk_image  # Empêche le garbage collection
-        image_label.update_idletasks()
+        image_label.image = tk_image
 
-def appliquer_filtre(filtre):
-    global photo_affichee, historique, indice_historique
-    if photo_affichee:
-        historique = historique[:indice_historique + 1]
-        photo_affichee = filtre(historique[indice_historique].copy())
-        historique.append(photo_affichee.copy())
-        indice_historique += 1
-        afficher_image()
+def appliquer_filtre_popup(filtre_func, nom_filtre):
+    def appliquer_et_afficher(intensite):
+        global photo_affichee, historique, indice_historique
+        if photo_affichee:
+            intensite_val = float(intensite)
+            historique = historique[:indice_historique + 1]
+            nouvelle = filtre_func(historique[indice_historique].copy(), intensite=intensite_val)
+            photo_affichee = nouvelle
+            historique.append(photo_affichee.copy())
+            indice_historique += 1
+            afficher_image()
+
+    popup = tk.Toplevel()
+    popup.title(f"{nom_filtre} - Intensité")
+    tk.Label(popup, text="Intensité :").pack()
+    slider = tk.Scale(popup, from_=1, to=10, orient="horizontal", resolution=1, command=appliquer_et_afficher)
+    slider.set(1)
+    slider.pack()
 
 def annuler():
     global indice_historique, photo_affichee
@@ -61,63 +63,36 @@ def ouvrir_image():
     if chemin:
         photo_originale = Image.open(chemin).convert("RGB")
         photo_affichee = photo_originale.copy()
-        historique = [photo_affichee.copy()]
+        historique.clear()
+        historique.append(photo_affichee.copy())
         indice_historique = 0
         afficher_image()
-
-def charger_image_par_defaut():
-    global photo_originale, photo_affichee, historique, indice_historique
-    chemin = "images/img.jpg"
-    if os.path.exists(chemin):
-        photo_originale = Image.open(chemin).convert("RGB")
-        photo_affichee = photo_originale.copy()
-        historique = [photo_affichee.copy()]
-        indice_historique = 0
-        afficher_image()
-    else:
-        print("Aucune image par défaut trouvée.")
 
 def lancer_interface():
     global image_label
-
     fenetre = tk.Tk()
     fenetre.title("UVSQolor")
-    fenetre.lift()
-    fenetre.attributes('-topmost', True)
-    fenetre.after(100, lambda: fenetre.attributes('-topmost', False))
 
-    # Menu principal
     menu = tk.Menu(fenetre)
     fenetre.config(menu=menu)
 
-    # Menu Fichier
     fichier_menu = tk.Menu(menu, tearoff=0)
     fichier_menu.add_command(label="Ouvrir", command=ouvrir_image)
     fichier_menu.add_command(label="Sauvegarder", command=sauvegarder_image)
-    fichier_menu.add_separator()
     fichier_menu.add_command(label="Quitter", command=fenetre.quit)
     menu.add_cascade(label="Fichier", menu=fichier_menu)
 
-    # Menu Édition
     edition_menu = tk.Menu(menu, tearoff=0)
     edition_menu.add_command(label="Annuler", command=annuler)
     edition_menu.add_command(label="Rétablir", command=retablir)
     menu.add_cascade(label="Édition", menu=edition_menu)
 
-    # Menu Filtres
     filtre_menu = tk.Menu(menu, tearoff=0)
-    filtre_menu.add_command(label="Flou uniforme", command=lambda: appliquer_filtre(filtre_flou_uniforme))
-    filtre_menu.add_command(label="Niveaux de gris", command=lambda: appliquer_filtre(filtre_niveaux_de_gris))
-    filtre_menu.add_command(label="Négatif", command=lambda: appliquer_filtre(filtre_negatif))
-    filtre_menu.add_command(label="Sépia", command=lambda: appliquer_filtre(filtre_sepia))
-    filtre_menu.add_command(label="Contraste (+ fort)", command=lambda: appliquer_filtre(filtre_contraste))
+    filtre_menu.add_command(label="Flou uniforme", command=lambda: appliquer_filtre_popup(filtre_flou_uniforme, "Flou"))
+    filtre_menu.add_command(label="Rehaussement", command=lambda: appliquer_filtre_popup(filtre_rehaussement, "Rehaussement"))
     menu.add_cascade(label="Filtres", menu=filtre_menu)
 
-    # Zone d'affichage
     image_label = tk.Label(fenetre)
     image_label.pack()
-
-    print("Interface prête.")
-    charger_image_par_defaut()
 
     fenetre.mainloop()
